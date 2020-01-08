fdccb405f9b2c6af438211aca062e19d3b524906
Roman <31464179+grand-roman@users.noreply.github.com>
2020-01-08 21:36:09 +0700

    fix

--- src/common.blocks/common-blocks.js
+++
@@ -1,7 +0,0 @@
-import accordionClickHandler from './e-accordion/e-accordion';
-
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('body').addEventListener('click', (event) => {
-        accordionClickHandler(event);
-    });
-});
--- src/index.js
+++
@@ -1,3 +0,0 @@
-import './styles.css';
-import './common.blocks/common-blocks';
-import './content.blocks/content-blocks'
